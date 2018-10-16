89085fb9744bf7d3443b0f80d77f5af2eaf22dd0
neha <sahuneha@seattleu.edu>
2018-10-15 21:16:40 -0700

    student json update

---
+++ studentdata.js
@@ -0,0 +1,132 @@
+{
+  "data": [
+    {
+      "student_id": "10027" ,
+      "student_name": "William Brown",
+      "dob": "09/02/1990",
+      "address": "1782-ELM SF CA ,12872",
+      "contact_no": "8898277288",
+      "department": "Computer Science"
+    },
+    {
+      "student_id": "10028",
+      "student_name": "John Smith",
+      "dob": "02/28/1992",
+      "address": "17828 Rosepark  SF CA ,18222",
+      "contact_no": "9998277288",
+      "department": "Information Science"
+    },
+    {
+      "student_id": "10029",
+      "student_name": "Gerald Robertson",
+      "dob": "11/01/1991",
+      "address": "201 ParkAvenue NE8 Seattle WA, 87872",
+      "contact_no": "9082772889",
+      "department": "Computer Science"
+    },
+    {
+      "student_id": "10030",
+      "student_name": "Maria Sharon",
+      "dob": "07/16/1996",
+      "address": "18712 Lakkhills APT 87 Kanab AZ ,98982",
+      "contact_no": "9202826622",
+      "department": "Mechanical Engineering"
+    },
+    {
+      "student_id": "10031",
+      "student_name": "Katrina Ratula",
+      "dob": "09/18/1992",
+      "address": "77282 Avenue 5 NE16 Bellevue WA,89822",
+      "contact_no": "7899877288",
+      "department": "Civil Engineering"
+    },
+    {
+      "student_id": "10032",
+      "student_name": "Rashi Sahu",
+      "dob": "09/21/1990",
+      "address": "108- Suncity Avenue 17 NE6 Bellevue WA, 89822",
+      "contact_no": "8898277288",
+      "department": "Computer Science"
+    },
+    {
+      "student_id": "10033",
+      "student_name": "Bernad Samuel",
+      "dob": "01/28/1991",
+      "address": "18781 Woodland Commons  George Square Utah ,18222",
+      "contact_no": "6992888564",
+      "department": "Electrical Engineering"
+    },
+    {
+      "student_id": "10034",
+      "student_name": "Jones Gerorge",
+      "dob": "04/06/1991",
+      "address": "501 Cherry Apartments Seattle WA, 87872",
+      "contact_no": "9082772889",
+      "department": "Electronics Engineering"
+    },
+    {
+      "student_id": "10035",
+      "student_name": "Bishop Murray",
+      "dob": "05/15/1992",
+      "address": "18712 Urban Heights APT 108 Kanab AZ ,98982",
+      "contact_no": "8902826622",
+      "department": "Information Science"
+    },
+    {
+      "student_id": "10036",
+      "student_name": "Tiffney Rose",
+      "dob": "02/28/1991",
+      "address": "72 Avenue 5 Croass-roads Bellevue WA,89822",
+      "contact_no": "3699877288",
+      "department": "Civil Engineering"
+    },
+    {
+      "student_id": "10037",
+      "student_name": "Racheal Clearfield",
+      "dob": "11/21/1987",
+      "address": "205 Palishades Avenue148 SE6 Seattle WA , 87872",
+      "contact_no": "8898277288",
+      "department": "Computer Science"
+    },
+    {
+      "student_id": "10038",
+      "student_name": "Anay Mehta",
+      "dob": "11/13/1988",
+      "address": "102 Shine Villas Avenue 138  SE10 Redmond WA ,18273",
+      "contact_no": "3349892292",
+      "department": "Computer Science"
+    },
+    {
+      "student_id": "10039",
+      "student_name": "Xi Lee",
+      "dob": "01/19/1991",
+      "address": "1928 ParkAvenue NE8 Seattle WA, 87872",
+      "contact_no": "4528772989",
+      "department": "Mechanical Engineering" 
+    },
+    {
+      "student_id": "10040",
+      "student_name": "George Grey",
+      "dob": "03/18/1993",
+      "address": "302 Jasminium Avenue 4 SE2 Seattle WA,87872",
+      "contact_no": "9202826622",
+      "department": "Mechanical Engineering"
+    },
+    {
+      "student_id": "10041",
+      "student_name": "Jackie Troy",
+      "dob": "12/31/1988",
+      "address": "201 Roystonea Avenue 5 NE16 Bellevue WA,89822",
+      "contact_no": "2199877288",
+      "department": "Civil Engineering"
+    },
+    {
+      "student_id": "10042",
+      "student_name": "Ronica Herbert",
+      "dob": "09/07/1987",
+      "address": "201 Cosoms Avenue 5 NE16 Bellevue WA,89822",
+      "contact_no": "6327728822",
+      "department": "Computer Engineering"
+    }
+  ]
+}
